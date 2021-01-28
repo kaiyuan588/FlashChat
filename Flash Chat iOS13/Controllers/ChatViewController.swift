9207e98cf34e090d5cb873674a12e160c598bc1c
kaiyuan zhao <kaiyuan941014@gmail.com>
2021-01-28 15:01:05 -0500

    sending messages

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
-
+import Firebase
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages : [Message] = []
+    
+    let db = Firestore.firestore()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        title = "FlashChat"
+        tableView.register(UINib(nibName: "MessageCell", bundle:  nil), forCellReuseIdentifier: "ReusableCell")
+        navigationItem.hidesBackButton = true
+        loadMessages()
+        
     }
-    
+    func loadMessages() {
+        
+        db.collection("messages").order(by: "date").addSnapshotListener { (querySnapshot, error) in
+            self.messages = []
+            if let e = error {
+                print("there were error\(e)")
+            }else {
+                if let snapshotDocument = querySnapshot?.documents {
+                    for doc in snapshotDocument {
+                        let data = doc.data()
+                        if let messageSender = data["sender"] as? String, let messageBody = data["body"] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection("messages").addDocument(data: ["sender": messageSender, "body": messageBody,"date": Date().timeIntervalSince1970]) { (error) in
+                if let e = error {
+                    print("error\(e)")
+                }else{
+                    print("success")
+                }
+            }
+        }
+        
     }
     
+    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+      
+    }
+    
+}
 
+extension ChatViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        
+        return cell
+    }
+}
+extension ChatViewController : UITableViewDelegate {
+    
 }
